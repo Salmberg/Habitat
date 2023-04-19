69d1cfec63f41981f15670a3a945a95b2ec5513f
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-19 12:31:30 +0200

    Initial Commit

---
+++ Habitat/HabitatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitatApp.swift
+//  Habitat
+//
+//  Created by David Salmberg on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct HabitatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
